7416cf32bf2c594725641a26c43cb3a28ba99fd5
jnmarcus <jnmarcus872@gmail.com>
2015-09-15 17:35:46 -0700

    add home route to appRoutes.js

--- app/appRoutes.js
+++ app/appRoutes.js
@@ -15,6 +15,19 @@ function appRoutes($stateProvider) {
   //  // }
   // };
 
+  var home = {
+    name: 'home', // state name
+    url: '/', // url path that activates this state
+    template: '<div home-view></div>', // generate the Directive "homeView" - when calling the directive in HTML, the name must not be camelCased
+    data: {
+      moduleClasses: 'page', // assign a module class to the <body> tag
+      pageClasses: 'home', // assign a page-specific class to the <body> tag
+      pageTitle: 'Home', // set the title in the <head> section of the index.html file
+      pageDescription: 'Meta Description goes here' // meta description in <head>
+    }
+  };
+
+
   var examplePage1 = {
     name: 'examplePage1',
     url: '/example-page-1',
@@ -41,7 +54,7 @@ function appRoutes($stateProvider) {
     }
   };
 
-  // $stateProvider.state(appMain);
+  $stateProvider.state(home);
   $stateProvider.state(examplePage1);
   $stateProvider.state(examplePage2);
 
--- app/modules/home/index.js
+++ app/modules/home/index.js
@@ -2,5 +2,5 @@
 // Home View
 module.exports = angular.module('modules.home', [])
     .directive('homeView', require('./homeDirective'))
-    .controller('HomeCtrl', require('./HomeController'))
-    .config(require('./homeRoutes'));
+    .controller('HomeCtrl', require('./HomeController'));
+    //.config(require('./homeRoutes'));
