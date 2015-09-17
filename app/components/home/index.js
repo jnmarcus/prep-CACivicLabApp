ac03918aea879050e1462add7065b8c20e6875b0
jnmarcus <jnmarcus872@gmail.com>
2015-09-16 18:26:29 -0700

    remove homeRoutes file and excess less files and references

--- app/components/home/index.js
+++ app/components/home/index.js
@@ -1,6 +1,5 @@
 'use strict';
 // Home View
-module.exports = angular.module('modules.home', [])
+module.exports = angular.module('components.home', [])
     .directive('homeView', require('./homeDirective'))
     .controller('HomeCtrl', require('./HomeController'));
-    //.config(require('./homeRoutes'));
